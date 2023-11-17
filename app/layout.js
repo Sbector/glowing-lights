abbfc97844feec49a27d63336f47ce369470eb4c
No_otra_vez <saulbector@gmail.com>
2023-11-17 15:16:54 -0600

    Change name

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Glowin lights',
-  description: 'Glowin lights that at some point will meet',
+  title: 'Subtle sync',
+  description: 'Glowin lights that at some point will sync',
 }
 
 export default function RootLayout({ children }) {
