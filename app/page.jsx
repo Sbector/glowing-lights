b076218b4cb1b38e3a28756e5a9bb5abac8f7873
No_otra_vez <saulbector@gmail.com>
2023-11-17 18:51:12 -0600

    Double tap message

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <>
       <Scene />
+      <div className="mensaje">
+        if(canvas.requestFullscreen!){
+          <h1 className='text-stone-800'>Double tap to fullscreen</h1>
+        }
+
+      </div>
     </>
   )
 }
